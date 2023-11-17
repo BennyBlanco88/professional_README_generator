6f4252c54ac12248cf83bbb62fe5505c12fb0ac5
BennyBlanco88 <ben.langs@yahoo.com>
2023-11-17 12:31:47 -0800

    changed Lincense to List type with choices

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ const questions = [
    message: "List your collaborators, if any, with links to their GitHub profiles.",
 },
 {
-    type: "input",
+    type: "list",
     name: "license",
     message: "The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).",
+    choices: ['B-DOG DeSigns'],
 },
 {
     type: "input",
